1b0e9d9cf42d37c88dc6529d57c9dd9a00b559db
Xiaohan Gao <gaolegao007@gmail.com>
2016-02-27 23:16:27 -0800

    Killing Enemies

--- WizardsInPyjamas/Assets/Scripts/Enemy.cs
+++ WizardsInPyjamas/Assets/Scripts/Enemy.cs
@@ -1,16 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class EnemyShoot : MonoBehaviour {
-	
+public class Enemy : MonoBehaviour {
+
 	public GameObject projectile;
 	public int minShotInterval;
 	public float damage;
 	public float angleVariance;
-
+	
 	private GameObject player;
 	private float lastTime;
-
+	
 	// Use this for initialization
 	void Start () {
 		lastTime = Time.time;
@@ -25,5 +25,10 @@ public class EnemyShoot : MonoBehaviour {
 			Vector3 rand = new Vector3(Random.value * angleVariance - angleVariance/2, Random.value * angleVariance - angleVariance/2, Random.value * angleVariance - angleVariance/2);
 			Instantiate(projectile, pos, Quaternion.LookRotation((player.transform.position - pos + rand).normalized));
 		}
+	}
+
+	public void hit()
+	{
+		GameObject.Find ("GameObject").gameObject.GetComponent<RandomEnemyGen> ().kill (this.gameObject);
 	}
 }
