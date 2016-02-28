fe5593bfce2ffbd693795df0560270d5b772d438
James <james534@outlook.com>
2016-02-28 13:12:16 -0500

    added drake model

--- WizardsInPyjamas/Assets/Scripts/Enemy.cs
+++ WizardsInPyjamas/Assets/Scripts/Enemy.cs
@@ -11,25 +11,93 @@ public class Enemy : MonoBehaviour {
 	
 	public GameObject player;
 	private float lastTime;
-	
-	// Use this for initialization
-	void Start () {
+
+    Animator anim;
+    float[] animLength = new float[4];  //0=idle, 1=death, 2=attack, 3=move
+    double runTime = 0;
+    int curAnim = 0;
+    bool dead = false;
+
+    void Start () {
 		lastTime = Time.time;
         player = GameObject.Find ("Player");
         p = player.GetComponent<Player>();
+
+        anim = GetComponent<Animator>();
+        RuntimeAnimatorController ac = anim.runtimeAnimatorController;
+        for (int i = 0; i < ac.animationClips.Length; i++)
+        {
+            if (ac.animationClips[i].name.Equals("Stand"))
+            {
+                animLength[0] = ac.animationClips[i].length;
+            }
+            else if (ac.animationClips[i].name.Equals("AttackUnarmed") ||
+               ac.animationClips[i].name.Equals("Attack2HL") ||
+               ac.animationClips[i].name.Equals("AttackOff"))
+            {
+                animLength[2] = ac.animationClips[i].length - 0.1f;
+            }
+            else if (ac.animationClips[i].name.Equals("Death"))
+            {
+                animLength[1] = ac.animationClips[i].length - 0.1f;
+            }
+            else if (ac.animationClips[i].name.Equals("Walk"))
+            {
+                animLength[3] = ac.animationClips[i].length - 0.1f;
+            }
+            else if (ac.animationClips[i].name.Equals("CombatWound") ||
+               ac.animationClips[i].name.Equals("StandWound"))
+            {
+                animLength[2] = ac.animationClips[i].length;
+            }
+            Debug.Log(ac.animationClips[i].name + " " + ac.animationClips[i].length);
+        }
+
     }
 	
 	// Update is called once per frame
 	void Update () {
 		if (Time.time > minShotInterval + lastTime + Random.value * 6 - 3) {
 			lastTime = Time.time;
-			Vector3 pos = transform.position + transform.forward;
+			Vector3 pos = transform.position + transform.forward*2;
 			Vector3 rand = new Vector3(Random.value * angleVariance - angleVariance/2, Random.value * angleVariance - angleVariance/2, Random.value * angleVariance - angleVariance/2);
-			Instantiate(projectile, pos, Quaternion.LookRotation((player.transform.position - pos + rand).normalized));
-		}
-	}
+            GameObject temp = Instantiate(projectile);
+            temp.transform.position = pos;
+            temp.transform.LookAt(player.transform.position);
 
-	public void hit(int mana)
+            SphereCollider temp2 = temp.GetComponent<SphereCollider>();
+            temp2.transform.SetParent(temp.transform);
+            Attack();
+        }
+        if (curAnim != 0)
+        {
+            if (anim.GetTime() - runTime > animLength[curAnim])
+            {
+                if (curAnim == 1)
+                {
+                    this.transform.position = new Vector3(-5000, -5000, 0);
+                    dead = true;
+                }
+
+                anim.Play("Idle");
+            }
+        }
+    }
+
+    public void Attack()
+    {
+        anim.Play("Attack");
+        runTime = anim.GetTime();
+        curAnim = 2;
+    }
+    public void Death()
+    {
+        anim.Play("Death");
+        runTime = anim.GetTime();
+        curAnim = 1;
+    }
+
+    public void hit(int mana)
 	{
 		GameObject.Find ("GameObject").gameObject.GetComponent<RandomEnemyGen> ().kill (this.gameObject);
         p.addMana(mana);
