ec7da7430e6e8c19f9c992efbd648f38993eec03
legacy1121 <nathankong11@yahoo.com>
2016-02-28 12:18:39 -0600

    Fire Orb and Attack Update
    Re-adjusted player size
    Added rotation and image to orb

---
+++ WizardsInPyjamas/Assets/Scripts/OrbSpin.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OrbSpin : MonoBehaviour
+{
+    void Update()
+    {
+        transform.Rotate(150 * Time.deltaTime, 200 * Time.deltaTime, 100 * Time.deltaTime);
+    }
+}
